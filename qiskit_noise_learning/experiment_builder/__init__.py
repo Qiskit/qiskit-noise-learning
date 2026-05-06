eb4ba2a7861bab0bb296452084fd9f69e706314a
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2026-05-06 11:42:18 -0400

    Add files primarily attributed to Daniel Puzzuoli
    This source code has been migrated from a private repository. Author is the most frequent contributor by blame line count across these files. Co-authors are all other contributors found in each file's git log history.
    
    Co-authored-by: Ian Hincks <ian.hincks@ibm.com>
    Co-authored-by: Samuele Ferracin <sam.ferracin@ibm.com>

---
+++ qiskit_noise_learning/experiment_builder/__init__.py
@@ -0,0 +1,21 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Experiment builder module."""
+
+from .experiment_builder import ExperimentBuilder
+from .experiment_generators import (
+    depth0_path_generator,
+    depth1_path_generator,
+    even_depth_pattern_generator,
+    even_depth_vanilla_pattern_generator,
+)
