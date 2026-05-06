9cf7fb751d64919eb023bd1d998226129bf713a9
Ian Hincks <ian.hincks@ibm.com>
2026-05-06 11:41:54 -0400

    Add files primarily attributed to Ian Hincks
    This source code has been migrated from a private repository. Author is the most frequent contributor by blame line count across these files. Co-authors are all other contributors found in each file's git log history.
    
    Co-authored-by: Daniel Puzzuoli <dan.puzzuoli@gmail.com>
    Co-authored-by: Samuele Ferracin <sam.ferracin@ibm.com>
    Co-authored-by: joshuasn <53916441+joshuasn@users.noreply.github.com>

---
+++ qiskit_noise_learning/analysis/__init__.py
@@ -0,0 +1,18 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from .analysis_pipeline import AnalysisPipeline, AnalysisStage
+from .average_observables import AverageObservables
+from .compute_observables import ComputeObservables
+from .curve_fit_observables import CurveFitObservables
+from .fit import Fit
+from .nnls_solve import NNLSSolve
