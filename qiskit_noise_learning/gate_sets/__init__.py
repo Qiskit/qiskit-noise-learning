50ee7599d19fcdc161688adc4b7c62933ec452a2
Ian Hincks <ian.hincks@ibm.com>
2026-05-06 11:41:50 -0400

    Add files primarily attributed to Ian Hincks
    This source code has been migrated from a private repository. Author is the most frequent contributor by blame line count across these files. Co-authors are all other contributors found in each file's git log history.
    
    Co-authored-by: Daniel Puzzuoli <dan.puzzuoli@gmail.com>
    Co-authored-by: Samuele Ferracin <sam.ferracin@ibm.com>

---
+++ qiskit_noise_learning/gate_sets/__init__.py
@@ -0,0 +1,20 @@
+# This code is a Qiskit project.
+#
+# (C) Copyright IBM 2026.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Gate set module."""
+
+from .gate import Gate
+from .gate_set import GateSet
+from .model_gate import ModelGate
+from .model_gate_set import ModelGateSet
+from .qiskit_gate import QiskitGate
+from .qiskit_gate_set import QiskitGateSet
