ed464761a3dabdae8496b9a278974850b39d49ed
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2026-05-28 10:16:44 -0700

    Update qiskit_noise_learning/sequences/path.py
    Co-authored-by: joshuasn <53916441+joshuasn@users.noreply.github.com>

--- qiskit_noise_learning/sequences/path.py
+++ qiskit_noise_learning/sequences/path.py
@@ -32,8 +32,7 @@ class Path(BaseSequence[FidelityIndex]):
         start_fragment: The start of the sequence.
         repeatable_fragment: The repeatable middle of the sequence.
         end_fragment: The end of the sequence.
-        depth: The number of repetitions of the repeatable fragment. If ``None``, the path
-            represents a variable-depth sequence.
+        depth: The number of repetitions of the repeatable fragment.
     """
 
     @property
