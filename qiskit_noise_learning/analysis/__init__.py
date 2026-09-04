917d444bc61e8f9761a203444083516601c789d3
Andrew Eddins <aeddins@ibm.com>
2026-09-04 16:57:46 -0700

    Unify NNLS, LSQLinear solvers as LeastSquaresSolve
    - Removed scipy.optimize.nnls solver, since that scales poorly.
    - Removed LSQLinear solver, replaced with more general/faster LeastSquaresSolver.
    - LeastSquaresSolver uses CVXPY if available (tends to be much faster), or else falls back to scipy lsq_linear solver (scales reasonably well, leveraging the sparse design-matrix provided by `IndexedMatrix`).
    - Also allows toggling the non-negativity constraint on the rates, which is sometimes useful.

--- qiskit_noise_learning/analysis/__init__.py
+++ qiskit_noise_learning/analysis/__init__.py
@@ -19,6 +19,6 @@ from .curve_fit_observables import CurveFitObservables
 from .fit import Fit
 from .flip_post_select import FlipPostSelect
 from .legacy import LegacySolve
-from .model_solve import LinearSystemData, LSQLinearSolve, NNLSSolve, PositivityMinSolve
+from .model_solve import LeastSquaresSolve, LinearSystemData, PositivityMinSolve
 from .symmetrize import SymmetrizeFidelities, SymmetrizeGenerators
 from .zero_post_select import ZeroPostSelect
